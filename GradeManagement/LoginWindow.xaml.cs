c91e603d8deae62c36c2b2490732fedd7cdbfe5a
2amStock <he181972dohuuduc@gmail.com>
2025-07-15 16:52:00 +0700

    login

--- GradeManagement/LoginWindow.xaml.cs
+++ GradeManagement/LoginWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GradeManagement.DAL.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,45 @@ namespace GradeManagement
     /// </summary>
     public partial class LoginWindow : Window
     {
+        GradeManagementSystemContext _context;
         public LoginWindow()
         {
             InitializeComponent();
+            _context = new GradeManagementSystemContext();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = pbPassword.Password;
+
+            if (username.IsNullOrEmpty() || password.IsNullOrEmpty())
+            {
+                MessageBox.Show("Nhập đầy đủ thông tin");
+                return;
+            }
+            var userAccount = _context.UserAccounts
+                .FirstOrDefault(u => u.Email == username && u.PasswordHash == password);
+            if (userAccount != null)
+            {
+                if (userAccount.Role.Equals("khaothi"))
+                {
+                    StudentWindow studentWindow = new StudentWindow();
+                    studentWindow.Show();
+                    this.Close();
+                }
+                else if (userAccount.Role.Equals("lecturer"))
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                } 
+                        
+            }
+            else
+            {
+                MessageBox.Show("Sai tk hoac mk"); return;
+            }
         }
     }
 }
